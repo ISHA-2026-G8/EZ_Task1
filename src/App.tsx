5c61665d8485704c2db1df2fe78ba6a6f3560b23
Isha Yadav <96190341+ISHA-2026-G8@users.noreply.github.com>
2026-01-31 12:59:03 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TreeView from './components/TreeView'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <TreeView />
+    </main>
+  )
+}
+
+export default App
